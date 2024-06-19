07be1bc5b7d559ed1b8a55b17a90551fe55971fe
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-19 17:32:33 +0530

    first commit

---
+++ components/ui/button/Index.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { VariantProps, cva } from "class-variance-authority"
+import clsx from "clsx"
+import { forwardRef } from "react"
+
+
+const variants = cva([
+
+], {
+    variants: {
+        variant: {
+            primary: [],
+            secondary: [],
+            destructive: [],
+            link:[],
+        },
+    },
+})
+
+const Loading = () => (
+    <div className="absolute inline-flex items-center">
+        <div className="w-4 h-4 rounded-full border-2 border-b-transparent animate-spin border-[inherit]"/>
+    </div>
+)
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+ const Button = forwardRef<HTMLButtonElement, Props>(function ButtonComponent
+    ({ className, children, loading, ...rest},
+         ref) {
+        return (
+            <button 
+                className={clsx(
+                'bg-indigo-500 text-white',
+                'w-[146px] h-[60px] leading-6',
+                'rounded-md',
+                'font-semibold tracking-wide',
+                'cursor-pointer',
+                'inline-flex items-center justify-center',
+                'relative shadow-md',
+                
+                //Hover
+                'transition',
+                'hover:bg-indigo-800 hover:shadow-lg',
+    
+                //Focus
+                'outline-none',
+                'ring-indigo-500/70 ring-offset-2 ',
+                'focus-visible:ring-2 focus:scale-[0.98]',
+    
+                //Disabled
+                'disabled:bg-indigo-300 disabled:cursor-not-allowed',
+                className)}
+                
+                {...rest}
+            >
+                {loading && <Loading /> }
+                <span
+                    className={clsx('transition', {
+                        'opacity-0': loading,
+                        'opacity-100': !loading,
+                    })}
+                >{children}</span>
+            </button>
+        ) 
+        
+    }
+ )
+
+ Button.displayName = 'Button'
+
+ export { Button }
