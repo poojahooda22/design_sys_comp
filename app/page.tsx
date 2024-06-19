84f830dea5165fdd944ed8dafc80bf1c9013cf8a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-19 20:54:23 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,18 @@ export default function Home() {
   return (
     <main className="mx-auto container mt-20 ">
       <div className="grid grid-cols-4 items-center justify-center gap-8">
-        <Button className="justify-self-center">
+        <Button className="justify-self-center" size="small">
           Primary
         </Button>
-
+        <Button className="justify-self-center" size="medium" disabled>
+          Primary
+        </Button>
+        <Button className="justify-self-center" size="large">
+          Primary
+        </Button>
+        <Button className="justify-self-center" size="xl">
+          Primary
+        </Button>
         <Button
            className="justify-self-center"
            loading={loading}
