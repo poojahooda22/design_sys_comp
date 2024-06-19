c7b5a1e6a4df794a0f37a4e3a30756b4bcfa1964
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-19 21:47:37 +0530

    added

--- components/ui/button/Index.tsx
+++ components/ui/button/Index.tsx
@@ -68,7 +68,7 @@ const variants = cva(
 const Loading = () => (
     <div className="absolute inline-flex items-center">
         <div 
-        className="w-4 h-4 rounded-full border-2 
+            className="w-4 h-4 rounded-full border-2 
             border-b-transparent animate-spin border-[inherit]"
         />
     </div>
