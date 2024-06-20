61c8dd78daa364fb584c7af5127778c02b8f3e26
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-20 15:44:02 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   return (
     <main className="mx-auto container mt-20 ">
-      <div className="grid grid-cols-4 items-center justify-center gap-8">
+      <div className="grid grid-cols-4 items-center justify-center gap-[4vw]">
         <Button className="justify-self-center" size="small">
           Primary
         </Button>
@@ -18,14 +18,67 @@ export default function Home() {
         <Button className="justify-self-center" size="large">
           Primary
         </Button>
-        <Button className="justify-self-center" size="xl">
-          Primary
-        </Button>
         <Button
            className="justify-self-center"
            loading={loading}
            onClick={() => setLoading(!loading)}
         >Primary</Button>
+        <Button
+          className="justify-self-center"
+          intent="secondary"
+          size="small"
+        >
+          Secondary
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="secondary"
+          size="medium"
+        >
+          Secondary
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="secondary"
+          size="large"
+        >
+          Secondary
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="secondary"
+          size="xl"
+        >
+          Secondary
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="destructive"
+          size="small"
+        >
+          Destructive
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="destructive"
+          size="medium"
+        >
+          Destructive
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="destructive"
+          size="large"
+        >
+          Destructive
+        </Button>
+        <Button
+          className="justify-self-center"
+          intent="destructive"
+          size="xl"
+        >
+          Destructive
+        </Button>
       </div>
 
     </main>
