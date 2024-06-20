84de0ac6fff49a7d9040fe3606def8a65f3c85a6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-20 21:01:10 +0530

    button updated

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -8,7 +8,7 @@ export default {
   component: Button,
   argTypes: {
     intent: {
-      options: ['primary', 'secondary', 'danger', 'warning', 'success'],
+      options: ['primary', 'secondary', 'danger', 'warning', 'succes'],
       control: { type: 'radio' }
     },
     size: {
