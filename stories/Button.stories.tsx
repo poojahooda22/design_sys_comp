01c41f53d04cbd1e50f8867179556d51a13044b2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-20 21:02:44 +0530

    added

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -8,7 +8,7 @@ export default {
   component: Button,
   argTypes: {
     intent: {
-      options: ['primary', 'secondary', 'danger', 'w', ''],
+      options: ['primary', 'secondary', 'danger', '', ''],
       control: { type: 'radio' }
     },
     size: {
