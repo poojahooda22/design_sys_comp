3a05ebe65ec3671a3e8492f36a63a109cbf910ec
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-20 15:46:55 +0530

    created button

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from '@/components/ui/button/Index';
+
+
+export default {
+  title: 'UI/Button',
+  component: Button,
+  argTypes: {
+    intent: {
+      options: ['primary', 'secondary', 'danger', 'warning', 'success'],
+      control: { type: 'radio' }
+    },
+    size: {
+      options: [ 'small', 'medium', 'large', 'xl'],
+      control: { type: 'radio' }
+    },
+    loading: {
+      control: { type: 'boolean' }
+    },
+    children: {
+      control: { type: 'text' }
+    }
+  }
+} as ComponentMeta<typeof Button>;
+
+
+
+const Template: ComponentStory<typeof Button> = (args: any) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  intent: 'primary',
+  children: 'Primary Button',
+};
