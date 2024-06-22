473d3eeaaa85f2442f410f083c78e0d5e2a1c8cb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-22 22:27:08 +0530

    added

--- app/ThemeContext.ts
+++ app/ThemeContext.ts
@@ -3,7 +3,7 @@
 import React, {useContext, createContext} from 'react';
 
 const ThemeContext 
-const ThemeUpdateContext 
+const ThemeUp 
 
 export function ThemeProvider({children} : any)  {
     const [darkTheme, setDarkTheme] = React.useState(true)
