79e10d310647720320ab4fe3074376b146c67ec7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-22 22:24:28 +0530

    added

--- app/ThemeContext.ts
+++ app/ThemeContext.ts
@@ -14,7 +14,7 @@ export function ThemeProvider({children} : any)  {
 
     return (
         <Themnt value={darkTheme}>
-            < value={to}
+            < value=}
                
             <
 
