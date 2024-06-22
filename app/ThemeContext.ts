1464ac175743e1486052e5906e2ac0b0926659e7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-22 22:21:12 +0530

    updated theme

--- app/ThemeContext.ts
+++ app/ThemeContext.ts
@@ -1,7 +1,24 @@
-import React from 'react';
+'use client'
 
-export default function App() {
-    
-                    
+import React, {useContext, createContext} from 'react';
+
+const ThemeContext = React.createContext();
+const ThemeUpdateContext = React.createContext();
+
+export function ThemeProvider({children} : any)  {
+    const [darkTheme, setDarkTheme] = React.useState(true)
+
+    function toggleTheme() {
+        setDarkTheme(prevDarkTheme => !prevDarkTheme)
+    }
+
+    return (
+        <ThemeContext.Provider value={darkTheme}>
+            <ThemeUpdateContext.Provider value={toggleTheme}>
+                {children}
+            </ThemeUpdateContext.Provider> 
+        </ThemeContext.Provider>
+
+    )
 }
 
