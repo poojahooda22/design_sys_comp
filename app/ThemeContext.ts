7e9eb487d91543de891679c79e16ff5ac1481c9b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-22 22:26:02 +0530

    added

--- app/ThemeContext.ts
+++ app/ThemeContext.ts
@@ -2,7 +2,7 @@
 
 import React, {useContext, createContext} from 'react';
 
-const ThemeContext = React.createContext();
+const ThemeContext = React.createContex
 const ThemeUpdateContext = React.createContext();
 
 export function ThemeProvider({children} : any)  {
