d5edd603882bcddeeede70d5868d191b00b7b9b0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-22 22:21:06 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,17 @@
-'use client'
+
 
 import { Button } from "@/components/ui/button/Index";
-import { useState } from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "./ThemeContext";
 
+export default function App() {
 
-export default function Home() {
-  const [loading, setLoading] = useState(false);
-
-  const [count, setCount] = useState()
-
-  function increment() {
-    setCount(prevCount )
-  }
-
-  function decrement() {
-    setCount(prevCount)
-  }
   return (
-    <main className="mx-auto container mt-20 text-white ">
-      {/* <div className="grid grid-cols-4 items-center justify-center gap-[4vw]">
-        <Button className="justify-self-center" size="small">
-          Primary
-        </Button>
-        <Button className="justify-self-center" size="medium" disabled>
-          Primary
-        </Button>
-        <Button className="justify-self-center" size="large">
-          Primary
-        </Button>
-        <Button
-           className="justify-self-center"
-           loading={loading}
-           onClick={() => setLoading(!loading)}
-        >Primary</Button>
-        <Button
-          className="justify-self-center"
-          intent="secondary"
-          size="small"
-        >
-          Secondary
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="secondary"
-          size="medium"
-        >
-          Secondary
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="secondary"
-          size="large"
-        >
-          Secondary
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="secondary"
-          size="xl"
-        >
-          Secondary
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="destructive"
-          size="small"
-        >
-          Destructive
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="destructive"
-          size="medium"
-        >
-          Destructive
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="destructive"
-          size="large"
-        >
-          Destructive
-        </Button>
-        <Button
-          className="justify-self-center"
-          intent="destructive"
-          size="xl"
-        >
-          Destructive
-        </Button>
-      </div> */}
-        <button 
-        onClick={decrement}
-        className="text-[1.5vw] mr-4 bg-blue-500 px-[2vw] rounded-lg">-</button>
-        <span className="text-[2vw] ">{count}</span>
-        <button 
-        onClick={increment}
-        className="text-[1.5vw] ml-4 bg-blue-500 px-[2vw] py-[0vw] rounded-lg">+</button>
-    </main>
+    <>
+      <ThemeProvider>
+        <button className="mt-14 ml-14">Toggle Button</button>
+      </Theme.Provider>
+    </>
+      
   );
 }
