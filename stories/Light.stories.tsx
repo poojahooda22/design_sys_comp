9d82a2f1d7ca14611448f7777894b1dafffe3fad
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-25 21:54:49 +0530

    updated stories

--- stories/Light.stories.tsx
+++ stories/Light.stories.tsx
@@ -12,5 +12,18 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
-    args: {},
+    args: {
+        variant: 'red'
+    },
+}
+
+export const Yellow: Story = {
+    args: {
+        variant: 'yellow'
+    },
+}
+export const Green: Story = {
+    args: {
+        variant: 'green'
+    },
 }
