6b809511c222769b472d024efbfc04bc18776b4e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-25 21:49:54 +0530

    added the buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,24 @@
 
 import { Button } from "@/components/ui/button/Index";
 import React, { useState } from "react";
-import { ThemeProvider } from "./ThemeContext";
+
 
 export default function App() {
 
   return (
     <>
-      <ThemeProvider>
-        <button className="mt-14 ml-14">Toggle Button</button>
-      </Theme.Provider>
+      <div className="w-full h-[100vh] bg-black text-white">
+        <div className="w-full h-full flex justify-center gap-8 items-center ">
+          <Button
+            intent="primary"
+            size="xl"
+          >Click me!</Button>
+          <Button
+            intent="destructive"
+            size="xl"
+          >Click me!</Button>
+        </div>
+      </div>
     </>
       
   );
