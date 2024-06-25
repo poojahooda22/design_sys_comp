3c94b6eeb3d49f4448e0dc396ca107d36a5c023f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-25 21:50:22 +0530

    new component light created

---
+++ components/ui/light/light.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {}
+
+const Light = ( props: Props) => {
+    return (
+        <div style={{
+            width: 70,
+            height: 70,
+            backgroundColor: "green",
+            borderRadius: "50%",
+        }}>   
+        </div>
+    )
+}
+
+export default Light
