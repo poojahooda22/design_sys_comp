d8997f8807173c7aaf9dabcda0eb1061887cf70d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-26 20:47:11 +0530

    updated from stories

--- stories/Form.stories.tsx
+++ stories/Form.stories.tsx
@@ -14,5 +14,13 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const  Base: Story = {};
+export const  Base: Story = {
+    play: async ({canvasElement}) => {
+        const canvas = within(canvasElement);
+
+        const submitBtn = canvas.getByText('Post question');
+
+        await expect(submitBtn).toBeInTheDocument();
+    }
+};
 
