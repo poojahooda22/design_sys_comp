ea297907c505290899018c395d1ab8556d34f3ec
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-06-26 20:29:37 +0530

    updated from stories

--- stories/Form.stories.tsx
+++ stories/Form.stories.tsx
@@ -2,4 +2,10 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import FormDemo from "@/components/ui/form";
 
+const meta: Meta<typeof FormDemo> = {
+    component: FormDemo,
+    title: 'Form'
+};
+
+
 
